5b2533fc3db6ec924336638d19dedd88dd5e6593
Igor <igorbaiosoares@gmail.com>
2020-03-15 22:44:10 -0300

    PercorreConjunto's implementation

--- Trabalho1/leituraDeTexto.py
+++ Trabalho1/leituraDeTexto.py
@@ -22,6 +22,8 @@ nomeConjunto = {"A","B","C","D","E","F","I","G","H","I","J","K","L","M","N","O",
 
 simbolos = {"=","{","}",",","\n","_", " "}
 simboloSinal = {"-","+"}
+numeros = {"1","2","3","4","5","6","7","8","9","0"}
+
 print("\n\n",nomeConjunto)
 teste = {textoArquivo}
 print("\n\n",teste)
@@ -30,11 +32,71 @@ conjuntos = []
 conjuntoComparativo = "A = {1,87,4,-1,43}"
 conjuntoComparativo2 = "{1,87,4,-1,43}"
 
+switcher = {
+        ",":"",
+        "{":"",
+        "}":""        
+        }
 
     
 def PercorreConjunto(textoArquivo):
        # a=""
     conjuntoAux = []
+    i = 0
+    print("total de elementos = ", (textoArquivo.count(",")+1))
+    print("ELementos: ")
+    onlyNumbers = ""
+    texto = textoArquivo.replace("}", "")
+    for item in texto.split(","):
+        if str.isdigit(item):
+            onlyNumbers += item
+            conjuntoAux.append(onlyNumbers)
+            onlyNumbers = "" 
+        else:
+            for character in item:
+                if str.isdigit(character):
+                    onlyNumbers += character
+                    conjuntoAux.append(onlyNumbers)
+                    onlyNumbers = "" 
+                elif character == "-":
+                    onlyNumbers = character
+
+            onlyNumbers = "" 
+    print(conjuntoAux)
+    '''while i < textoArquivo.__len__():
+        #virgula = simbolos.intersection(textoArquivo[i])
+        letraConjunto = nomeConjunto.intersection(textoArquivo[i])
+        valorDuplo1 = textoArquivo[i+1]
+        valorDuplo2 = textoArquivo[i+2]
+        if not nomeConjunto.intersection(textoArquivo[i]):
+            if textoArquivo[i] == "}":
+                print("Número de elementos do conjunto",conjuntoAux, "é:" ,textoArquivo.count(",")+1)
+                
+                listaDeConjuntos.append(conjuntos)
+                #if textoArquivo[i].__eq__("_"):
+                 #   print("Número de conjuntos é:", listaDeConjuntos.__len__())
+                break 
+            elif not simbolos.intersection(textoArquivo[i]):
+                conjuntos.append(textoArquivo[i])
+            elif switcher.get(simbolos.intersection(textoArquivo[i])):
+                if textoArquivo[i+1] == numeros.intersection(textoArquivo[i+1]).__str__() and textoArquivo[i+2] == numeros.intersection(textoArquivo[i+2]).__str__():
+                     valorDuplo = textoArquivo[i+1]+textoArquivo[i+2]
+                     conjuntoAux.append(valorDuplo)
+        ''' '''
+            elif simboloSinal.intersection(conjunto):
+                a += conjunto
+                if not simboloSinal.intersection(a):
+                    conjuntos.append(a)'''
+            
+            #i +=1
+          
+        #else:
+            
+        #    if not conjuntos.__contains__(letraConjunto):
+        #          conjuntoAux.append(letraConjunto)
+        #          i += 1
+        
+'''
     for conjunto in textoArquivo:
         letraConjunto = nomeConjunto.intersection(conjunto)
         if not nomeConjunto.intersection(conjunto):
@@ -45,17 +107,17 @@ def PercorreConjunto(textoArquivo):
                     print("Número de conjuntos é:", listaDeConjuntos.__len__())
                     break 
                 continue
-            elif conjunto != " " and not simbolos.intersection(conjunto):
+            elif not simbolos.intersection(conjunto):
                 conjuntos.append(conjunto)
-            '''elif simboloSinal.intersection(conjunto):
-                a += conjunto
-                if not simboloSinal.intersection(a):
-                    conjuntos.append(a)'''
+            #elif simboloSinal.intersection(conjunto):
+            #    a += conjunto
+            #    if not simboloSinal.intersection(a):
+            #        conjuntos.append(a)
         else:
             
             if not conjuntos.__contains__(letraConjunto):
                   conjuntoAux.append(letraConjunto)
-              
+   '''           
 
 def Pertence(numero, conjunto):
     if conjunto.__contains__(numero):
@@ -110,6 +172,7 @@ def PercorreNomeConjunto(nomeConjunto):
 def Menu(conjuntoComparativo, conjuntoBase):
     print("**************************************\n")
     print("OPERAÇÕES:\n")
+    print("0 - Percorre\n")
     print("1 - Pertence\n")
     print("2 - Não pertence\n")
     print("3 - Contido ou igual \n")
@@ -125,7 +188,9 @@ def Menu(conjuntoComparativo, conjuntoBase):
 
     valorMenu = int(input("Digite o valor correspondente a operação desejada: "))
 
-    if valorMenu == 1:
+    if valorMenu == 0:
+        PercorreConjunto(textoArquivo)
+    elif valorMenu == 1:
         Pertence("1", textoArquivo)
     elif valorMenu == 2:
         Pertence("2", textoArquivo)
