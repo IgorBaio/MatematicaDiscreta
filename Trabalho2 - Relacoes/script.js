f4f6e8a69edb7bd2aa78295dabdb7b6136c1399c
Igor <igorbaiosoares@gmail.com>
2020-04-03 12:29:07 -0300

    start second work, test insert from windows

---
+++ Trabalho2 - Relacoes/script.js
@@ -0,0 +1,15 @@
+function getMyProgram(arquivo1,arquivo2){
+ var file1 = document.getElementById("arquivo1").value;
+ console.log(file1);
+
+
+}
+
+function getFile1(){
+    var x = document.getElementById("file");
+    x = open(x.value);
+    console.log(x);
+    console.log("aaaa");
+    document.write(x);
+    // var x  = window.File
+}
